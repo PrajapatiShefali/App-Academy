b0ca97fb4b24b26c1487fc8c62cce64520bccb5e
Shefali Prajapati <shefali24@somaiya.edu>
2023-05-06 21:20:34 +0530

    Revising OBjects,TDD with its projects and learn HTTP servers

--- practice-for-week-04-tdd-style-long-practice/problems/person.js
+++ practice-for-week-04-tdd-style-long-practice/problems/person.js
@@ -1,48 +1,89 @@
 class Person {
- 
-    constructor(name,age){
-      this.name=name;
-      this.age=age
-    }
-    sayHello(){
-      return `Hello +${this.name}`
-    }
-    visit(otherPerson){
-      return `${this.name}+ visited +${otherPerson.name}`;
-    }
-    swicthVisit(otherPerson){
-      otherPerson.visit(this)
-    }
-    update(newPerson){
-      if (typeof newPerson !== 'object') {
-        throw TypeError("Type must be an object");
-      }
-      if (!newPerson.hasOwnProperty("name")) {
-        throw TypeError("Input must include a name property");
-      }
-      if (!newPerson.hasOwnProperty("age")) {
-        throw TypeError("Input must include a age property");
-      }
-      this.name = newPerson.name;
-      this.age = newPerson.age;
-    }
-    tryUpdate(object){
-      try{
-        this.update(object)
-        
-      }
-      catch(e){
-        return false
-      }
-      return true 
-    }
-    static greetAll(people){
-      let result=[];
-      for (let i = 0; i < people.length; i++) {
-        result.push(people[i].sayHello());
-      }
-      return result;
-    }
-}
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
+  /**
+   * The function sayHello() is a method of the class Person. It takes no arguments and returns a
+   * string
+   * @returns The result of the function is the string "Hi, I'm ${this.name}"
+   */
+  sayHello() {
+    const result = `Hi, I'm ${this.name}`;
+    console.log(result);
+    return result;
+  }
+
+  /**
+   * The visit function takes in an object and returns a string that says the name of the person who
+   * called the function visited the name of the person passed in as an argument.
+   * @param other - The object that this object is going to visit.
+   * @returns The string `${this.name} visited ${other.name}`
+   */
+  visit(other) {
+    return `${this.name} visited ${other.name}`;
+  }
+
+  /**
+   * "If the other object is a subclass of this class, then call the visit function of the other object
+   * with this object as the argument."
+   * 
+   * The above function is a bit confusing, so let's break it down
+   * @param other - The other object to visit.
+   * @returns The other.visit(this) is returning the result of the visit method of the other object.
+   */
+  switchVisit(other) {
+    return other.visit(this);
+  }
+
+  /**
+   * The function takes an object as an argument, and if the object has a name and age property, it
+   * updates the name and age properties of the Person object
+   * @param newPerson - This is the object that will be used to update the person.
+   */
+  update(newPerson) {
+    if (typeof newPerson !== 'object') {
+      throw TypeError("Type must be an object");
+    }
+    if (!newPerson.hasOwnProperty("name")) {
+      throw TypeError("Input must include a name property");
+    }
+    if (!newPerson.hasOwnProperty("age")) {
+      throw TypeError("Input must include a age property");
+    }
+    this.name = newPerson.name;
+    this.age = newPerson.age;
+  }
+
+  /**
+   * If the update function throws an error, return false, otherwise return true
+   * @param newPerson - The new person object to be updated.
+   * @returns The return value is a boolean.
+   */
+  tryUpdate(newPerson) {
+    try {
+      this.update(newPerson);
+    } catch (error) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Given an array of people, return an array of greetings.
+   * @param people - an array of Person objects
+   * @returns An array of strings.
+   */
+  static greetAll(people) {
+    const result = [];
+    for (let i = 0; i < people.length; i++) {
+      result.push(people[i].sayHello());
+    }
+    return result;
+  }
+}
+const person = new Person("Mai", 38);
+const person2 = new Person("Erin", 42);
+Person.greetAll([person, person2]);
 module.exports = Person;
--- practice-for-week-04-tdd-style-long-practice/problems/triangle.js
+++ practice-for-week-04-tdd-style-long-practice/problems/triangle.js
@@ -1,1 +1,110 @@
-// Your code here
+// Your code here/* A triangle has three sides, a perimeter, and can be validated. */
+class Triangle {
+    constructor(side1, side2, side3) {
+      this.side1 = side1;
+      this.side2 = side2;
+      this.side3 = side3;
+    }
+  
+    /**
+     * It returns the perimeter of the triangle.
+     * 
+     * Returns:
+     *   The perimeter of the triangle.
+     */
+    getPerimeter() {
+      return this.side1 + this.side2 + this.side3;
+    }
+  
+    /**
+     * If the sum of any two sides is greater than the third side, then the triangle is valid.
+     * 
+     * Returns:
+     *   a boolean value.
+     */
+    hasValidSideLengths() {
+      if (this.side1 + this.side2 > this.side3) return true;
+      if (this.side1 + this.side3 > this.side2) return true;
+      if (this.side2 + this.side3 > this.side1) return true;
+      return false;
+    }
+  
+    /**
+     * The function validate() is called when the user clicks the button. It calls the function
+     * hasValidSideLengths() and assigns the result to the variable isValid.
+     */
+    validate() {
+      this.isValid = this.hasValidSideLengths();
+    }
+  
+    /**
+     * It takes an array of triangles and returns an array of triangles that have valid side lengths
+     * 
+     * Args:
+     *   triangles: An array of Triangle objects.
+     * 
+     * Returns:
+     *   An array of triangles that have valid side lengths.
+     */
+    static getValidTriangles(triangles) {
+      return triangles.filter(triangle => triangle.hasValidSideLengths());
+    }
+  }
+  
+  /* The Triangle class is the parent class of the Scalene and Isosceles classes. The Scalene and
+  Isosceles classes inherit the properties and methods of the Triangle class. */
+  class Scalene extends Triangle {
+    constructor(side1, side2, side3) {
+      super(side1, side2, side3);
+    }
+  
+    /**
+     * If the sides are not equal, then it is scalene.
+     * 
+     * Returns:
+     *   The boolean value of the expression.
+     */
+    isScalene() {
+      if (this.side1 === this.side2) return false;
+      if (this.side1 === this.side3) return false;
+      if (this.side2 === this.side3) return false;
+      return true;
+    }
+  
+    /**
+     * The function isValidScalene is set to the value of the function isScalene.
+     */
+    validate() {
+      this.isValidScalene = this.isScalene();
+    }
+  }
+  
+  /* The Isosceles class is a subclass of the Triangle class. It has a constructor that takes three
+  arguments, and it has a method called validate that checks to see if the triangle is an isosceles
+  triangle */
+  class Isosceles extends Triangle {
+    constructor(side1, side2, side3) {
+      super(side1, side2, side3);
+    }
+  
+    /**
+     * If any of the sides are not equal, return false. Otherwise, return true.
+     * @returns The boolean value of the expression.
+     */
+    isIsosceles() {
+      if (this.side1 !== this.side2) return false;
+      if (this.side1 !== this.side3) return false;
+      if (this.side2 !== this.side3) return false;
+      return true;
+    }
+  
+    /**
+     * The function isValidIsosceles is a boolean that returns true if the triangle is an isosceles
+     * triangle.
+     */
+    validate() {
+      this.isValidIsosceles = this.isIsosceles();
+    }
+  }
+  
+  module.exports = { Triangle, Scalene, Isosceles };
